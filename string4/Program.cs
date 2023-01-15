10f55dae14ba5a1e8a346354f3da52dcd2c477ab
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-15 14:33:29 +0100

    Add files via upload

---
+++ string4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = s1.Substring(0, 1) + s1.Substring(s1.Length - 1, 1);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+    }
+}
